b3b021e5123eacb9b842b9758f9c9a0e1790cda9
omvikram <omvikram@hotmail.com>
2022-03-30 21:06:31 +0530

    added design problem file

---
+++ Guest Chat - Design Problem.py
@@ -0,0 +1,7 @@
+# How will you create a ChitChat system between 2 customers?
+# How will you push the chat notification to multiple client devices of same user?
+# How can you extend this to multi room chat system?
+# What all infrastructure you will use and why?
+# How can you broadcast some of the specific messages?
+# What will be the mechasimn to safeguard PII information?
+# How can you moderate the obscene and faulty words (automatic)
