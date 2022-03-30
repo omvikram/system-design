373262bb96e8c46e4fa5bfe6a82b997b4265e507
omvikram <omvikram@hotmail.com>
2022-03-30 21:24:25 +0530

    added problem file

---
+++ Multilingual System - Design Problem.py
@@ -0,0 +1,7 @@
+# How can you build a multilingual system which translates the input to configured language?
+# Do you know some existing library which does this in the industry?
+# Please design the system keeping in mind bith Online translation + Manual Translation is possible
+# This should be centralised system which can scale upto 4X the traffic
+# The system should be scalable enough to translate any new language in future with minial changes
+# Which ISO version you will prefer to during the implementation?
+# If caching is used then for how long you want to keep the data and why?
