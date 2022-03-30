9b7ed2bc2ca2aae8e0fc69652cf741314cbbb0f3
omvikram <omvikram@hotmail.com>
2022-03-30 21:13:06 +0530

    added problem statement file

---
+++ Middleware - BFF Design Problem.py
@@ -0,0 +1,7 @@
+# What is the need of a middleware?
+# What will be your preferred tech stack of the middleware?
+# How it is different than wrapper API layer?
+# How can you profile different response for different clients?
+# What are the pros and cons of the Middleware Layer?
+# How do you maintain the authN & authZ across middleware layer and microservices?
+# What is the role difference between Backend for Frontend and Middleware layer?
