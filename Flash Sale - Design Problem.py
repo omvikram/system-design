534cdf7bd40b8d2ed8fea1913d2fb3c21658926b
omvikram <omvikram@gmail.com>
2022-03-30 20:50:12 +0530

    adding problem file

---
+++ Flash Sale - Design Problem.py
@@ -0,0 +1,5 @@
+# If there is a FLASH SALE going to happen in an e-commerce platform Lets say FLASH10 (10% off)
+# What all things you will keep in mind for sudden system load?
+# What all services gets impacted?
+# How will you predict the load?
+# How will you make sure the promotion is getting executed in runtime for 4x of customers?
