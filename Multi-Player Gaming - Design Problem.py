68763674ed47763bbe86970892bf5f09eb697740
omvikram <omvikram@hotmail.com>
2022-03-30 21:19:00 +0530

    made file changes

---
+++ Multi-Player Gaming - Design Problem.py
@@ -0,0 +1,7 @@
+# Build a multi player online gaming system
+# How frequently you can push the event from one device to another?
+# How do you want to manage the data storage for the system?
+# How do you trigger the latest update of the app versions?
+# Which important microservices system you would like to have in this system?
+# How can you connect 2 online users to each other in this system?
+# What will be the bottleneck if you want to scale this to millions of users.
