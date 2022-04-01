c349ae4dffc428a882ec8f52513876b0854550a1
omvikram <omvikram@hotmail.com>
2022-04-01 14:03:18 +0530

    added OTA Loyalty probelm statement

---
+++ OTA Loyalty - Design Problem.py
@@ -0,0 +1,11 @@
+# Design a system where OTAs like MMT, Booking, Expedia wants to run a Loyalty program for hoteliers
+# This should benefit the hoteliers to enroll for a program and they should be able to increase repeated customers
+# This should be extensible to any loyalty mode for eg. credit card, club membership or hotel special beenefits etc.
+# Customers should be able to see the benefits on the top of the funnel 
+# Hotel Supply should be able to configure them from backend by targeting certain segments of the customers
+# Every time customer books a hotel inventory he/she should be able to maintain a history of loyalty points
+# There should be a band of loyalty program also and it should be configurable for eg. 
+#   5 trips - Silver Band
+#   10 trips - Gold Band
+#   25 trips - Diamond Band 
+#   50 trips - Platinum Band etc
