2011280071f480e816fd72881931bbf0be2d5832
omvikram <omvikram@hotmail.com>
2022-04-01 14:18:59 +0530

    added Signalling system statement

---
+++ Signalling System - Design Problem.py
@@ -0,0 +1,6 @@
+# Given Indian road system from Left to Right and RED-Orange-Green Signal on Traffic Signal Route
+# If as per the US rule we change the signal system from Right to Left and Trafic Signal color code remains the same
+# What all challenge be there in the Traffic Signal automated system
+# Can you please DB design/ER Diagram the same and show an HLD to understand the architecture which can handle both the logic
+# Let the Left to Right and Right to Left road system be the configuration
+# Try to make it as generic and as real time as it can be. Thank You.
