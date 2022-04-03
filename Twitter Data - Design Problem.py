2841885ce7b543c8a864e9a880e0ab441a880655
omvikram <omvikram@hotmail.com>
2022-04-04 00:27:35 +0530

    twitter problem

---
+++ Twitter Data - Design Problem.py
@@ -0,0 +1,8 @@
+#How to build a system which can take care of 1000 Req/sec and process the tweets and find out if the tweet contains
+#COVID positive
+#COVID negative
+#Given that you have an API provided by Twitter to fetch the COVID related tweets.
+#How do we fetch all the tweets without missing a single tweet with the help of the API?
+#What will be the logic/algo of the Classifier Service to classify between COVID positive & COVID negative tweets/counts?
+#How will you scale the database and what will be your database choice for this system?
+
