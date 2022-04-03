f70c9b2440d151793bcaa02cef880d190cdb9759
omvikram <omvikram@hotmail.com>
2022-04-04 00:17:04 +0530

    data aggregation system

---
+++ Data Agg System - Design Problem.py
@@ -0,0 +1,8 @@
+# Given a highly transactional system build a aggregator system
+# which can process high influx of the transactiond data
+# as well as it should be able to aggregate the data for analytics
+# There can be different templates of aggregation data 
+# There can be a combination of static and dynamic data for aggregation
+# We should also be able to incorporate a new set of templates with configurable data points for aggregation
+# You are free to choose the DB and Caching infra of your choice. Pipeline can be also be another option.
+# Lets build a system which can aggreagte the data and can be used by any internal system for aggreation.
