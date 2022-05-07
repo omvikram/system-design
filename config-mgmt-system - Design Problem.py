b78dc87e3942a83d228cd2f0336cf189e319a761
omvikram <omvikram@hotmail.com>
2022-05-07 09:39:52 +0530

    config mgmt system

---
+++ config-mgmt-system - Design Problem.py
@@ -0,0 +1,8 @@
+## Desing a centralised Config Mgmt System
+
+## It should have its own database and config portal
+## It should be able to store key value pair with JSON support
+## It should be faster to access from clients
+## It should be easy to update or delete some keys
+## It should be able to scale for another applications too
+## It should be able to run along with secretkeymanager
