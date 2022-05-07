1d286845bfb995e8d5c40c0606028e9c01afe6a6
omvikram <omvikram@hotmail.com>
2022-05-07 09:26:01 +0530

    update route problem

---
+++ Update Route - Design Problem.py
@@ -0,0 +1,11 @@
+## Problem Solving & System Design
+ 
+## I asked him to design a solution where a GO-MMT Cars/Bike traveller who is travelling between A-B (source-destination) 
+## founds a roadblock/pothole/construction work due to which the traveller needs to change the direction, 
+## how can he update GO-MMT app and notify rest of the customers who is also planning to travel the same A-B route on the very same day.
+
+# How to save Route information
+# How to find other travellers in the same route
+# How to notify other customers
+# How to suggest the alternate route
+# How to scale the Notification queue if there are 50K incidents happen in a worst case scenario?
