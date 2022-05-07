787039acf0edc748c1f15d345c677362fbdcb772
omvikram <omvikram@hotmail.com>
2022-05-07 08:55:41 +0530

    handshake probklems

---
+++ AirBnB Handshake - Design Problem.py
@@ -0,0 +1,7 @@
+## Consider GO-MMT shares hand with AirBnB and want to sell their inventory in Goibibo & MakeMyTrip.
+ 
+## How can we sell the inventory at GO-MMT end but we should NOT share the customer details during booking push to AirBnB.
+## How can we send a communication like “How to reach the property” which AirBnB sends to its customer, 
+## We want to send the same via Goibibo or MakeMyTrip, how can we achieve this without sharing customer details while consuming same info from AirBnB.
+## How can you handle the bulk event push to GO-MMT system with push notifications for daily customers?
+## You are allowed to think both the approaches "PULL vs PUSH" as well you can request AirBnB to make some changes too
---
+++ Auth Handshake - Design Problem.py
@@ -0,0 +1,7 @@
+## AuthN & AuthZ Deep Dive Problem
+
+## Consider you have an account in mydrive.com and you want to access mydrive.com from another application photoeditor.com
+## You need to access mydrive.com photos services from photoeditor.com only not any other services
+## How do you authenticate the application photoeditor.com to access mydrive.com first
+## How do you authenticate the user from photoeditor.com to mydrive.com
+## How do you restrcit the access to only the photos services of mydrive.com 
