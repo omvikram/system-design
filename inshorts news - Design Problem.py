78413c145a8baf03dccab6aaf07b54c5524e8be6
omvikram <omvikram@hotmail.com>
2022-05-07 09:32:49 +0530

    inshorts news problem

---
+++ inshorts news - Design Problem.py
@@ -0,0 +1,9 @@
+## Create a Summary News App like InShorts
+
+## Scrapping Blocked News
+## How to find Duplicate News
+## How to find recent news which is not present in the system
+## How to make it as recent as possible but as fast as possible
+## How to support multiple clients (Web. MWeb & Apps)
+## How can you optimise the summarisation of the news
+## How can you easily parse the data of new news source
