bc30e0531c386df5b362e658c059a9874f0f3564
omvikram <omvikram@hotmail.com>
2022-05-07 09:22:40 +0530

    paypal integration

---
+++ PayPal Integration - Design Problem.py
@@ -0,0 +1,11 @@
+## System Design of PayPal Integration
+ 
+## Build a system which integrates with PayPal sandbox and handles all type of failure cases. Let’s build it together.
+## How can you avoid man in the middle attack?
+## How can you pass the data from client to server side
+## How will you handle time out issue?
+## How will you handle late payment cases?
+## How will you handle pending payment cases for customers?
+## In the absence of "status check" api how will you handle the pending case scenario?
+## PUSH vs PULL Mechanism with PayPal
+## What is the use of Callback APIs
