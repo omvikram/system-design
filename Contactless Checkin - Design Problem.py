f7f94e49d5441f334b3975aa74d3176631aa918c
omvikram <omvikram@hotmail.com>
2022-05-07 09:16:44 +0530

    contactless checkin problem

---
+++ Contactless Checkin - Design Problem.py
@@ -0,0 +1,12 @@
+## System Design Challenge
+## Design a system where we can allow contactless checkin for GOIBIBO & MMT hotel customers. (Don't give much info other than this one liner statement)
+ 
+## The candidate should ask several question about the scope of the problem.
+## Clarify if you want to do contactless checkin only for Hotels or for flights also in future.
+## What all you documents you want to collect during normal checkin from customers in hotel.
+## Do you want to use automated process to verify a government document.
+## Which all documents you want to save in our system or use third party service for that.
+## What is the frequency when you wan to push notifications to customer for contactless checkin.
+## How does the Hotelier Accept/Reject flow for the documents will work?
+## How do you maintain the security of the whole process?
+## Which all third party resoiurces or APIs you would like to use here?
