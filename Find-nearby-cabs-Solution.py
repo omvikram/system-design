b3ad267c585cce70babc0ab459719af849614caf
omvikram <omvikram@hotmail.com>
2022-05-16 12:19:51 +0530

    find-nearby-cabs-problem

---
+++ Find-nearby-cabs-Solution.py
@@ -0,0 +1,19 @@
+## Step 1: Constraints and use cases
+## How many cabs are there in the country/state/possibly in one city?
+## How many customers look for the cabs at a same time?
+## How many requests our database can take - IOPS?
+## How many requests the given API can take - RPM?
+## Can we store pincodes or number plates in our databse?
+## What all attributes I can save for a cab in my database?
+## What all information is required to book a cab?
+
+## Step 2: Abstract design (High Level Design)
+## TODO
+
+## Step 3: Understanding bottlenecks
+## Can I make 10000 lat long comparison using the same API?
+## Is my database capable of taking so many requests for so many customers at a same time?
+##   
+
+## Step 4: Scaling your abstract design (Low Level Design)
+## TODO
