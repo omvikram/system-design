0543dafc61920591fad28aea3443387af236fbe4
omvikram <omvikram@hotmail.com>
2022-05-21 12:22:29 +0530

    contact-tracing-problem

---
+++ Contact-Tracing-App - Design Problem.py
@@ -0,0 +1,9 @@
+## High Level System Design for Contact Tracing App (like Aarogya Setu)
+ 
+## Problem statement is build the system design for a contact tracing app which can do the following –
+ 
+## Take care of user identification uniquely considering 1 Cr+ users
+## Track the location/GPS of the device for last 14 days
+## Share the contact of the users who come close to the device and share/save the user id
+## On claiming the user is COVID +ve application should be able to quickly find last 14 days users who came close to the device
+## Notify all the users who were in close proximity of the device in last 14 days
