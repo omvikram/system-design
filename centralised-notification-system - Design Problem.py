f117b4071e4a66f45f9f0e17bee45d428d8f645d
omvikram <omvikram@hotmail.com>
2022-06-09 13:46:43 +0530

    cns system design

---
+++ centralised-notification-system - Design Problem.py
@@ -0,0 +1,9 @@
+## System Design of Centralised Notification Service
+ 
+## Design a centralised notification for GO-MMT keeping Security & Scalibility in mind. Also take care of the following areas –
+ 
+## How will you segregate different vertical/product communications?
+## How will you prioritise the communication packet?
+## How can we easily update customer details?
+## How can you schedule a communication?
+## How can you broadcast a message to multiple customers?
