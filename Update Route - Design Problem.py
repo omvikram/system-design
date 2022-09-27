2581d21a0c22ad565f4f6000fd98d8e734161d1b
omvikram <omvikram@hotmail.com>
2022-09-27 14:55:09 +0530

    Update Update Route - Design Problem.py

--- Update Route - Design Problem.py
+++ Update Route - Design Problem.py
@@ -5,8 +5,8 @@
 ## how can the driver update GO-MMT app about the blocker on the route and 
 # how can GO-MMT notify rest of the customers who is also planning to travel via the same blocker route on the very same day.
 
-# How to save Route information
+# How to save Route information with blocker 
 # How to find other travellers in the same route
-# How to notify other customers
 # How to suggest the alternate route
+# How to fecth and notify other customers
 # How to scale the Notification queue if there are 50K incidents happen in a worst case scenario?
